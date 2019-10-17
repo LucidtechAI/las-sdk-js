d1880d83b406eb4d66f6d7bea4c6917c6327c6de
Nikita Antonenkov <krigar1184@gmail.com>
2019-10-17 12:55:08 +0300

    Eslint setup

--- packages/las-sdk-core/src/client.ts
+++ packages/las-sdk-core/src/client.ts
@@ -1,114 +1,114 @@
-import axios, {AxiosRequestConfig} from 'axios';
-import {Credentials} from './credentials';
+import axios, { AxiosRequestConfig } from 'axios';
+import { Credentials } from './credentials';
 
 export class Client {
     apiEndpoint: string;
+
     credentials: Credentials;
 
     constructor(apiEndpoint: string, credentials: Credentials) {
-        this.apiEndpoint = apiEndpoint;
-        this.credentials = credentials;
+      this.apiEndpoint = apiEndpoint;
+      this.credentials = credentials;
     }
 
     getDocument(documentId: string) {
-        return this.makeGetRequest(`/documents/${documentId}`);
+      return this.makeGetRequest(`/documents/${documentId}`);
     }
 
     postDocuments(content: string, contentType: string, consentId: string) {
-        const body = {
-            'content': Buffer.from(content).toString('base64'),
-            'contentType': contentType,
-            'consentId': consentId
-        };
+      const body = {
+        content: Buffer.from(content).toString('base64'),
+        contentType,
+        consentId,
+      };
 
-        return this.makePostRequest('/documents', body);
+      return this.makePostRequest('/documents', body);
     }
 
     postPredictions(documentId: string, modelName: string) {
-        const body = {
-            'documentId': documentId,
-            'modelName': modelName
-        };
+      const body = {
+        documentId,
+        modelName,
+      };
 
-        return this.makePostRequest('/predictions', body);
+      return this.makePostRequest('/predictions', body);
     }
 
     getProcesses() {
-        return this.makeGetRequest('/processes');
+      return this.makeGetRequest('/processes');
     }
 
     postProcesses(stateMachineArn: string, inputData: any) {
-        const body = {
-            'stateMachineArn': stateMachineArn,
-            'inputData': inputData
-        };
+      const body = {
+        stateMachineArn,
+        inputData,
+      };
 
-        return this.makePostRequest('/processes', body);
+      return this.makePostRequest('/processes', body);
     }
 
     postTasks(activityArn: string) {
-        const body = {
-            'activityArn': activityArn
-        };
+      const body = {
+        activityArn,
+      };
 
-        return this.makePostRequest('/tasks', body);
+      return this.makePostRequest('/tasks', body);
     }
 
     patchTasks(taskId: string, taskResult: any) {
-        const body = {
-            'taskResult': taskResult
-        };
+      const body = {
+        taskResult,
+      };
 
-        return this.makePatchRequest(`/tasks/${taskId}`, body);
+      return this.makePatchRequest(`/tasks/${taskId}`, body);
     }
 
     makeGetRequest(path: string) {
-        return this.makeAuthorizedRequest(axios.get, path);
+      return this.makeAuthorizedRequest(axios.get, path);
     }
 
     makeDeleteRequest(path: string) {
-        return this.makeAuthorizedRequest(axios.delete, path);
+      return this.makeAuthorizedRequest(axios.delete, path);
     }
 
     makePostRequest(path: string, body: any) {
-        return this.makeAuthorizedRequest(axios.post, path, body);
+      return this.makeAuthorizedRequest(axios.post, path, body);
     }
 
     makePatchRequest(path: string, body: any) {
-        return this.makeAuthorizedRequest(axios.patch, path, body);
+      return this.makeAuthorizedRequest(axios.patch, path, body);
     }
 
     private makeAuthorizedRequest(axiosFn: (url: string, body?: any, config?: AxiosRequestConfig) => Promise<any>, path: string, body?: any) {
-         return new Promise<any>((resolve, reject) => {
-             const endpoint = `${this.apiEndpoint}${path}`;
-             this.getAuthorizationHeaders().then(headers => {
-                 const config = {headers: headers};
-                 const handle = !!body ? () => axiosFn(endpoint, body, config) : () => axiosFn(endpoint, config);
+      return new Promise<any>((resolve, reject) => {
+        const endpoint = `${this.apiEndpoint}${path}`;
+        this.getAuthorizationHeaders().then((headers) => {
+          const config = { headers };
+          const handle = body ? () => axiosFn(endpoint, body, config) : () => axiosFn(endpoint, config);
 
-                 handle().then(response => {
-                     resolve(response.data);
-                 }).catch(error => {
-                     reject(error);
-                 })
-             }).catch(error => {
-                 reject(error);
-             })
+          handle().then((response) => {
+            resolve(response.data);
+          }).catch((error) => {
+            reject(error);
+          });
+        }).catch((error) => {
+          reject(error);
         });
+      });
     }
 
     private getAuthorizationHeaders(): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            this.credentials.getAccessToken().then(accessToken => {
-                const headers = {
-                    'X-Api-Key': this.credentials.apiKey,
-                    'Authorization': `Bearer ${accessToken}`
-                };
+      return new Promise<any>((resolve, reject) => {
+        this.credentials.getAccessToken().then((accessToken) => {
+          const headers = {
+            'X-Api-Key': this.credentials.apiKey,
+            Authorization: `Bearer ${accessToken}`,
+          };
 
-                resolve(headers);
-            }).catch(error => {
-                reject(error);
-            });
+          resolve(headers);
+        }).catch((error) => {
+          reject(error);
         });
+      });
     }
 }
-
--- packages/las-sdk-core/src/credentials.ts
+++ packages/las-sdk-core/src/credentials.ts
@@ -3,57 +3,61 @@ import { ITokenStorage } from './storage';
 
 export class Token {
     readonly accessToken: string;
+
     readonly expiration: number;
+
     readonly refreshToken?: string;
 
     isValid() {
-        return Date.now() < this.expiration;
+      return Date.now() < this.expiration;
     }
 
     constructor(accessToken: string, expiration: number, refreshToken?: string) {
-        this.accessToken = accessToken;
-        this.expiration = expiration;
-        this.refreshToken = refreshToken;
+      this.accessToken = accessToken;
+      this.expiration = expiration;
+      this.refreshToken = refreshToken;
     }
 }
 
 export abstract class Credentials {
     readonly apiKey: string;
+
     protected token?: Token;
+
     protected storage?: ITokenStorage<Token>;
 
     protected constructor(apiKey: string, storage?: ITokenStorage<Token>) {
-        this.apiKey = apiKey;
-        this.storage = storage;
+      this.apiKey = apiKey;
+      this.storage = storage;
     }
 
     getAccessToken(): Promise<string> {
-        const storage = this.storage;
+      const { storage } = this;
 
-        return new Promise<string>((resolve, reject) => {
-            let token = this.token || null;
+      return new Promise<string>((resolve, reject) => {
+        let token = this.token || null;
 
-            if (!(token && token.isValid()) && storage) {
-                token = storage.getPersistentToken();
-            }
+        if (!(token && token.isValid()) && storage) {
+          token = storage.getPersistentToken();
+        }
 
-            if (token && token.isValid()) {
-                this.token = token;
-                return resolve(token.accessToken);
-            }
+        if (token && token.isValid()) {
+          this.token = token;
+          return resolve(token.accessToken);
+        }
 
-            this.getToken().then(newToken => {
-                this.token = newToken;
+        this.getToken().then((newToken) => {
+          this.token = newToken;
 
-                if (storage) {
-                  storage.setPersistentToken(newToken);
-                }
+          if (storage) {
+            storage.setPersistentToken(newToken);
+          }
 
-                resolve(newToken.accessToken);
-            }).catch(error => {
-                reject(error);
-            });
-        })
+          resolve(newToken.accessToken);
+        }).catch((error) => {
+          reject(error);
+        });
+      });
     }
 
     protected abstract getToken(): Promise<Token>
