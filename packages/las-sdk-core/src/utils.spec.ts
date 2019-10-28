275cc21c009588a2ffedefafb6c38ecbbc1e66fa
Nikita Antonenkov <krigar1184@gmail.com>
2019-10-28 16:43:39 +0300

    Fix query string encoding

--- packages/las-sdk-core/src/utils.spec.ts
+++ packages/las-sdk-core/src/utils.spec.ts
@@ -5,9 +5,10 @@ test('Builds URL', () => {
   const fixtures = [
     ['http://localhost/', 'http://localhost/'],
     ['http://localhost/?key=value', 'http://localhost/', { key: 'value' }],
-    ['http://localhost/?key=value1,value2', 'http://localhost/', { key: ['value1', 'value2'] }],
-    ['http://localhost/?key=value1%20value2%20value3', 'http://localhost/', { key: encodeURIComponent('value1 value2 value3') }],
-    ['http://localhost/?key=+asdf/qwerty=', 'http://localhost/', { key: '+asdf/qwerty=' }],
+    ['http://localhost/?key1=value1&key2=value2', 'http://localhost/', { key1: 'value1', key2: 'value2' }],
+    ['http://localhost/?key=value1%2Cvalue2', 'http://localhost/', { key: ['value1', 'value2'] }],
+    ['http://localhost/?key=value1%20value2%20value3', 'http://localhost/', { key: 'value1 value2 value3' }],
+    ['http://localhost/?key=%2Basdf%2Fqwerty%3D', 'http://localhost/', { key: '+asdf/qwerty=' }],
   ];
 
   fixtures.forEach((fixture: any[]) => {
