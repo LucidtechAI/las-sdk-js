e7b3f87774fb76d90445ef3df24e6650448be426
Roger Andersen <hexjelly@hexjelly.com>
2020-11-04 15:35:38 +0100

    group tests by method

--- packages/las-sdk-core/src/client.spec.ts
+++ packages/las-sdk-core/src/client.spec.ts
@@ -1,69 +1,88 @@
 import { default as uuidv4 } from 'uuid/v4';
-import { getTestClient } from './helpers.spec';
+import { getTestClient } from './helpers';
 
+let client = getTestClient();
 
-test('Testing successful createDocument', async () => {
-  const client = getTestClient();
-
-  const testContent = uuidv4();
-  const testContentType = 'image/jpeg';
-  const testConsentId = uuidv4();
-  const testBatchId = uuidv4();
-  const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId, testBatchId);
-  await expect(createDocumentPromise).resolves.toHaveProperty('consentId');
-  await expect(createDocumentPromise).resolves.toHaveProperty('contentType');
-  await expect(createDocumentPromise).resolves.toHaveProperty('documentId');
+beforeEach(() => {
+  client = getTestClient();
 });
 
-test('Testing erroneous createDocument', async () => {
-  const client = getTestClient();
+describe('createDocument', () => {
+  test('valid request body', async () => {
+    const testContent = uuidv4();
+    const testContentType = 'image/jpeg';
+    const testConsentId = `las:consent:${uuidv4().replace(/-/g, '')}`;
+    const testBatchId = `las:batch:${uuidv4().replace(/-/g, '')}`;
+    const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId, testBatchId);
+    await expect(createDocumentPromise).resolves.toHaveProperty('consentId');
+    await expect(createDocumentPromise).resolves.toHaveProperty('contentType');
+    await expect(createDocumentPromise).resolves.toHaveProperty('documentId');
+  });
 
-  const testContent = uuidv4();
-  const testContentType = 'erroneousContentType';
-  const testConsentId = uuidv4();
-  const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId);
-  await expect(createDocumentPromise).rejects.toBeDefined();
+  test('invalid Content-Type', async () => {
+    const testContent = uuidv4();
+    const testContentType = 'erroneousContentType';
+    const testConsentId = uuidv4();
+    const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId);
+    await expect(createDocumentPromise).rejects.toBeDefined();
+  });
+
+  test('invalid consentId pattern', async () => {
+    const testContent = uuidv4();
+    const testContentType = 'image/jpeg';
+    const testConsentId = uuidv4();
+    const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId);
+    await expect(createDocumentPromise).rejects.toBeDefined();
+  });
+
+  test('invalid batchId pattern', async () => {
+    const testContent = uuidv4();
+    const testContentType = 'image/jpeg';
+    const testConsentId = uuidv4();
+    const createDocumentPromise = client.createDocument(testContent, testContentType, testConsentId);
+    await expect(createDocumentPromise).rejects.toBeDefined();
+  });
 });
 
-test('Testing successful createPrediction', async () => {
-  const client = getTestClient();
+describe('createPredictions', () => {
+  test('valid request', async () => {
+    const testDocumentId = `las:document:${uuidv4().replace(/-/g, '')}`;
+    const testModelId = `las:model:${uuidv4().replace(/-/g, '')}`;
+    const createPredictionPromise = client.createPrediction(testDocumentId, testModelId);
+    await expect(createPredictionPromise).resolves.toHaveProperty('documentId');
+    await expect(createPredictionPromise).resolves.toHaveProperty('predictions');
+  });
 
-  const testDocumentId = uuidv4();
-  const testModelName = 'invoice';
-  const createPredictionPromise = client.createPrediction(testDocumentId, testModelName);
-  await expect(createPredictionPromise).resolves.toHaveProperty('documentId');
-  await expect(createPredictionPromise).resolves.toHaveProperty('predictions');
+  test('invalid model name', async () => {
+    const testDocumentId = `las:document:${uuidv4().replace(/-/g, '')}`;
+    const testModelId = 'erroneousModelId';
+    const createPredictionPromise = client.createPrediction(testDocumentId, testModelId);
+    await expect(createPredictionPromise).rejects.toBeDefined();
+  });
 });
 
-test('Testing erroneous createPrediction', async () => {
-  const client = getTestClient();
-
-  const testDocumentId = uuidv4();
-  const testModelName = 'erroneousModelName';
-  const createPredictionPromise = client.createPrediction(testDocumentId, testModelName);
-  await expect(createPredictionPromise).rejects.toBeDefined();
+describe('getDocument', () => {
+  test('valid request', async () => {
+    const documentId = uuidv4();
+    const getDocumentPromise = client.getDocument(documentId);
+    await expect(getDocumentPromise).resolves.toHaveProperty('consentId');
+    await expect(getDocumentPromise).resolves.toHaveProperty('documentId');
+    await expect(getDocumentPromise).resolves.toHaveProperty('contentType');
+  });
 });
 
-test('Testing successful getDocument', async () => {
-  const client = getTestClient();
-
-  const documentId = uuidv4();
-  const getDocumentPromise = client.getDocument(documentId);
-  await expect(getDocumentPromise).resolves.toHaveProperty('consentId');
-  await expect(getDocumentPromise).resolves.toHaveProperty('documentId');
-  await expect(getDocumentPromise).resolves.toHaveProperty('contentType');
+describe('listDocuments', () => {
+  test('valid request', async () => {
+    const testBatchId = uuidv4();
+    const listDocumentsPromise = client.listDocuments(testBatchId);
+    await expect(listDocumentsPromise).resolves.toBeDefined();
+  });
 });
 
-test('Testing listDocuments', async () => {
-  const client = getTestClient();
-  const testBatchId = uuidv4();
-  const listDocumentsPromise = client.listDocuments(testBatchId);
-  await expect(listDocumentsPromise).resolves.toBeDefined();
-});
-
-test('Testing createBatch', async () => {
-  const client = getTestClient();
-  const description = "I am going to create a new batch, give me a batch ID!"
-  const createBatchPromise = client.createBatch(description);
-  await expect(createBatchPromise).resolves.toHaveProperty('batchId');
+describe('createBatch', () => {
+  test('valid request', async () => {
+    const description = 'I am going to create a new batch, give me a batch ID!';
+    const createBatchPromise = client.createBatch(description);
+    await expect(createBatchPromise).resolves.toHaveProperty('batchId');
+  });
 });
