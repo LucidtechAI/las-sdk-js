f2fc9b6950cedfcbbff1e59888834d91ab94f3e0
augustak <august@lucidtech.ai>
2021-04-13 16:34:41 +0200

    Name changes in Model: created -> createdTime, updated -> updatedTime

--- packages/las-sdk-core/src/types.ts
+++ packages/las-sdk-core/src/types.ts
@@ -306,7 +306,7 @@ export type Field = {
 }
 
 export type Model = {
-  created: string | null;
+  createdTime: string | null;
   description: string | null;
   fieldConfig: Record<string, Field> | null;
   height: number;
@@ -314,7 +314,7 @@ export type Model = {
   name: string | null;
   preprocessConfig: PreprocessConfig;
   status: 'active' | 'training';
-  updated: string | null;
+  updatedTime: string | null;
   width: number;
 }
 
