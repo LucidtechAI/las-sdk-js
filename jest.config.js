3aa13af589e028e050cc474cee0f817c92444cf8
Roger Andersen <roger@lucidtech.ai>
2021-12-10 10:28:01 +0100

    add coverage script

--- jest.config.js
+++ jest.config.js
@@ -2,34 +2,28 @@ module.exports = {
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   verbose: true,
-  collectCoverageFrom: ['packages/**/*.ts'],
-  coveragePathIgnorePatterns: [
-    "jest.config.js",
-    "/node_modules/",
-    "/lib/",
-  ],
+  collectCoverageFrom: ['packages/**/*.ts', '!packages/**/lib/**/*.{js,ts}', '!packages/**/*.spec.*'],
+  coveragePathIgnorePatterns: ['jest.config.js', '<rootDir>/**/node_modules/', '<rootDir>/**/lib/'],
   moduleNameMapper: {
-    '^@lucidtech/(.*)$': '<rootDir>/packages/$1/'
+    '^@lucidtech/(.*)$': '<rootDir>/packages/$1/',
   },
-  "projects": [
+  projects: [
     {
-      "preset": "ts-jest",
-      "displayName": "core",
-      "testMatch": ["<rootDir>/packages/las-sdk-core/**/*.spec.ts"]
+      preset: 'ts-jest',
+      displayName: 'core',
+      testMatch: ['<rootDir>/packages/las-sdk-core/**/*.spec.ts'],
     },
     {
-      "preset": "ts-jest",
-      "displayName": "node",
-      "testEnvironment": "node",
-      "testMatch": ["<rootDir>/packages/las-sdk-node/**/*.spec.ts"]
+      preset: 'ts-jest',
+      displayName: 'node',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/packages/las-sdk-node/**/*.spec.ts'],
     },
     {
-      "preset": "ts-jest",
-      "displayName": "browser",
-      "testEnvironment": "jsdom",
-      "testMatch": ["<rootDir>/packages/las-sdk-browser/**/*.spec.ts"]
-    }
-  ]
-
+      preset: 'ts-jest',
+      displayName: 'browser',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/packages/las-sdk-browser/**/*.spec.ts'],
+    },
+  ],
 };
-
