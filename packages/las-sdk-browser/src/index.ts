d23d00560f3f1a86851e4f480474945d033102b8
augustak <august@lucidtech.ai>
2025-06-18 15:38:03 +0200

    Remove browser auth

--- packages/las-sdk-browser/src/index.ts
+++ packages/las-sdk-browser/src/index.ts
@@ -1,4 +1,4 @@
-export { AuthorizationCodeCredentials, PKCEDerived } from './credentials';
+export { TokenCredentials } from './credentials';
 export { SessionStorage } from './storage';
 export { Client, Credentials, Token, TokenStorage } from '@lucidtech/las-sdk-core';
 export * from '@lucidtech/las-sdk-core';
